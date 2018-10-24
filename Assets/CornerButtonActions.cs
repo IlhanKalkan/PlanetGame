fed9a787dbb44882133c382c155c442327b9c322
Ilhan <ilhandelilkalkan@gmail.com>
2018-10-24 15:01:09 +0200

    Resource UI looks nicer (WIP)

---
+++ Assets/CornerButtonActions.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CornerButtonActions : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    public GameObject panel;
+
+    private bool clicked = false;
+
+    public void OnClick()
+    {
+        if (!panel.activeSelf)
+        {
+            panel.SetActive(!panel.activeSelf);
+        }
+        clicked = !clicked;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        panel.SetActive(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (!clicked)
+        {
+            panel.SetActive(false);
+        }
+    }
+}
